f9ab0661183827a21ee6540e524a8e35b52ac922
Kayahan24 <kayahansan24@gmail.com>
2023-02-27 14:40:01 +0300

    First

---
+++ App.tsx
@@ -0,0 +1,115 @@
+import React, {useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import PhoneInput from './lib/PhoneInput';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const [value, setValue] = useState('');
+  const [countryCode, setCountryCode] = useState('');
+  const [formattedValue, setFormattedValue] = useState('');
+  const [valid, setValid] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const phoneInput = useRef<PhoneInput>(null);
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <SafeAreaView style={styles.wrapper}>
+          {showMessage && (
+            <View style={styles.message}>
+              <Text>Country Code : {countryCode}</Text>
+              <Text>Value : {value}</Text>
+              <Text>Formatted Value : {formattedValue}</Text>
+              <Text>Valid : {valid ? 'true' : 'false'}</Text>
+            </View>
+          )}
+          <PhoneInput
+            ref={phoneInput}
+            defaultValue={value}
+            defaultCode="TR"
+            layout="first"
+            onChangeText={(text) => {
+              setValue(text);
+            }}
+            onChangeFormattedText={(text) => {
+              setFormattedValue(text);
+              setCountryCode(phoneInput.current?.getCountryCode() || '');
+            }}
+            countryPickerProps={{withAlphaFilter:true}}
+            withDarkTheme
+            withShadow
+            autoFocus
+          />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              const checkValid = phoneInput.current?.isValidNumber(value);
+              setShowMessage(true);
+              setValid(checkValid ? checkValid : false);
+              setCountryCode(phoneInput.current?.getCountryCode() || '');
+              let getNumberAfterPossiblyEliminatingZero = phoneInput.current?.getNumberAfterPossiblyEliminatingZero();
+              console.log(getNumberAfterPossiblyEliminatingZero);
+            }}>
+            <Text style={styles.buttonText}>Gönder</Text>
+          </TouchableOpacity>
+          
+        </SafeAreaView>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lighter,
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    borderRadius:30,
+    marginTop: 20,
+    height: 50,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F78E1E',
+    shadowColor: 'rgba(0,0,0,0.4)',
+    shadowOffset: {
+      width: 1,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 14,
+    fontWeight:'bold',
+  },
+  redColor: {
+    backgroundColor: '#F57777'
+  },
+  message: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+});
+
+export default App;
